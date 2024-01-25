6bd7cd201f9cb039341c3d6af3206e22346276a4
SheepSupreme <lukas.nick5@gmail.com>
2024-01-25 19:40:32 +0100

    it should work now

--- sketch_jan10a/AKS/src/display.cpp
+++ sketch_jan10a/AKS/src/display.cpp
@@ -1,16 +1,43 @@
+
 #include "display.h"
 
+display::display() : display(128, 64, &Wire, -1) {
 
-display::display()
-{
-    Adafruit_SSD1306::Adafruit_SSD1306(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 }
 
-display::update_display(int input)
+void display::begin()
 {
-    Adafruit_SSD1306::clearDisplay();
-    Adafruit_SSD1306::setCursor(10,10);
-    // Adafruit_SSD1306::setFont(NULL);         TODO Font
-    Adafruit_SSD1306::setTextSize(3);
-    Adafruit_SSD1306::write(input);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+}
+
+
+void display::update_display(String input, uint8_t x, uint8_t y, uint8_t size)
+{
+  display.clearDisplay();
+  display.setCursor(x,y);
+  display.setFont();
+  display.setTextSize(size);
+  display.setTextColor(SSD1306_WHITE);
+  display.print(input);
+  display.display();
+}
+
+void display::status_errorRange()
+{
+  display.update_display("move not\n possible",15 ,10, 2);
+  delay(2000);
+  display.update_display("range\n violation",35 ,10, 2);
+}
+
+void display::status_position(int position)
+{
+  
+}
+
+void display::status_calibration()
+{
+
 }
--- sketch_jan10a/AKS/src/display.h
+++ sketch_jan10a/AKS/src/display.h
@@ -1,19 +1,27 @@
 #ifndef display_h
 #define display_h
 
-#include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
 #include <Wire.h>
+#include <SPI.h>
+#include <Fonts/Picopixel.h>
+#include <string>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET     -1 
+#define SCREEN_ADDRESS 0x3C
 
 class display{
-    public:
-        display();
-        update_display(int input);
-    private:
-        uint8_t SCREEN_WIDTH = 128;
-        uint8_t SCREEN_HEIGHT = 64;    
-        int8_t OLED_RESET = -1;
-        uint8_t SCREEN_ADDRESS = 0x3C;
-}
+  public:
+    display();
+    void update_display(String input, uint8_t x, uint8_t y, uint8_t size);
+    void status_calibration();
+    void status_position(int position);
+    void begin();
+  private:
+    Adafruit_SSD1306 display;
+};
 
-#endif 
+#endif
