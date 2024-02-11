510846b48fa71b9c010146018f968889500a7e11
SheepSupreme <lukas.nick5@gmail.com>
2024-02-11 15:21:37 +0100

    fdsa

--- sketch_jan10a/GUI.py
+++ sketch_jan10a/GUI.py
@@ -2,12 +2,25 @@ import customtkinter
 import serial
 
 #Serial setup
-serial_port = 'COM3'
+serial_port = 'COM4'
 baud_rate = 9600
 ser = serial.Serial(serial_port, baud_rate)
 
 #Customtkinter setup
 root = customtkinter.CTk()
+
+class Button:
+    def __init__(self,root,name,_row,_column):
+        self.root = root
+        self.name = name
+        self.button = customtkinter.CTkButton(master=self.root, text = self.name, command=self.send_text)
+        self.button.grid(row = _row, column = _column, padx=10,pady=10)
+
+    def send_text(self):
+        message = self.name
+        print(message)
+        message_bytes = message.encode('utf-8')
+        ser.write(message_bytes)
 
 class App:
     def __init__(self,root):
@@ -15,14 +28,8 @@ class App:
         self.root.title('GUI')
         self.root.geometry('400x150')
         self.root.columnconfigure(0,weight=1)
-
-        self.button = customtkinter.CTkButton(self.root, text='blink',command = self.blink)
-        self.button.grid(row=0,column=0,padx=10,pady=10)
-    
-    def blink(self):
-        message = 'blink'
-        message_bytes = message.encode('utf-8')
-        ser.write(message_bytes)
+        self.button_calibrate = Button(self.root,'calibrate',0,0)
+        self.button_blink = Button(self.root,'blink',1,0)
 
 App(root)
 
